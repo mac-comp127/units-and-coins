b84561aa446144438a8caa78568b19eb83d51686
Paul Cantrell <cantrell@pobox.com>
2020-09-07 00:50:37 -0500

    VS Code + Gradle migration

--- test/edu/macalester/comp127/hw1/UnitConversionsTest.java
+++ test/edu/macalester/comp127/hw1/UnitConversionsTest.java
@@ -1,14 +1,17 @@
 package edu.macalester.comp127.hw1;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitConversionsTest {
     @Test
     public void testSheppeyToMiles() {
+        // The last argument, the 0.00001, specifies a margin of error. This is useful
+        // with floating point numbers, where rounding errors can produce results that
+        // are off by just a tiny amount.
         assertEquals(28.0, UnitConversions.sheppeyToMiles(32), 0.00001);
-        assertEquals(86.2749999, UnitConversions.sheppeyToMiles( 98.6), 0.00001);
+        assertEquals(86.2749999, UnitConversions.sheppeyToMiles(98.6), 0.00001);
         assertEquals(185.5, UnitConversions.sheppeyToMiles(212), 0.00001);
         assertEquals(100, UnitConversions.sheppeyToMiles(114.285714), 0.00001);
     }
